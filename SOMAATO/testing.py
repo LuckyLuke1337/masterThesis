245ee33b97a5b5c20887e54905e846605b7703ce
LuckyLuke1337 <lukas@wolfit.cz>
2024-03-28 11:51:32 +0100

    improved testing functions

--- SOMAATO/testing.py
+++ SOMAATO/testing.py
@@ -1,22 +1,28 @@
-import main as iSOMA
+import main as SOMAATO
 import statistics
 from tabulate import tabulate
 
-data=[["function", "mean", "stddev"]]
+
 fitness = 0
+parameters = [[0.1,2.1,0.11]]
 
-for costFunction in range(1,31):
-    mean = 0
-    values =[]
-    for i in range(1,51):
-        fitness = iSOMA.main(10, 0.3, 10, 5, 15, "test.dat", costFunction)
-        mean += fitness
-        values.append(fitness)
-    print(values)    
-    data.append(["f"+str(costFunction), mean/51, statistics.stdev(values)])
-    print("calculated ", costFunction, " of 30" )
 
-print(tabulate(data,headers='firstrow',tablefmt='grid'))
+for inputs in parameters:
+    data=[["function", "mean", "stddev"]]
+    for costFunction in range(1,31):
+        mean = 0
+        values = []
+
+        for i in range(1,51):
+            fitness = SOMAATO.main(*inputs, "test.dat", costFunction)
+            mean += fitness
+            values.append(fitness)
+            
+        data.append(["f"+str(costFunction), mean/51, statistics.stdev(values)])
+        print("calculated ", costFunction, " of 30 ", end="\r")
+
+    print("table for input values: ",inputs)
+    print(tabulate(data,headers='firstrow',tablefmt='grid'))
 
 
     
