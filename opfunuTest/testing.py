eb945cd090fc66f435e8b2b988432b2555c93da4
LuckyLuke1337 <lukas@wolfit.cz>
2024-04-03 18:56:18 +0200

    fixed bugs

--- opfunuTest/testing.py
+++ opfunuTest/testing.py
@@ -7,8 +7,10 @@ import opfunu
 
 costFunctions = opfunu.get_functions_based_classname("2017")
 
+
+index = 0
 fitness = 0 
-parameters = [[45,10,4,10]]
+parameters = [[23,6,3,10]]
 
 
 for inputs in parameters:
@@ -16,13 +18,14 @@ for inputs in parameters:
     for costFunction in costFunctions:
         mean = 0
         values = []
-        for i in range(1,51):
+        for i in range(1,6):
             fitness = SOMAT3A.main(*inputs, "test.dat", costFunction.__name__)
             mean += fitness
             values.append(fitness)
             
-        data.append([str(costFunction.__name__), mean/51, statistics.stdev(values)])
-        print("calculated ", costFunction, " of 30 ", end="\r")
+        data.append([str(costFunction.__name__), mean/5, statistics.stdev(values)])
+        index += 1
+        print("calculated ", index, " of ", len(costFunctions), end="\r")
 
     print("table for input values: ",inputs)
     print(tabulate(data,headers='firstrow',tablefmt='grid'))
